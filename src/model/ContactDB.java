bbdd07ea3ea6029570b5f01c3b2ff11c8e67be0f
Unknown <mpagustoni@inf.ufrgs.br>
2017-07-15 00:57:51 -0300

    inicio codigo de arquivos

--- src/model/ContactDB.java
+++ src/model/ContactDB.java
@@ -1,8 +1,41 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class ContactDB {
-	private ArrayList <Contact> contactList;
+
+	
+	private String path = "data";
+	private String filename = "contacts";
+	
+	public ArrayList<Contact> getContacts(){
+        try {
+        	FileInputStream arq = new FileInputStream(path +"/" + filename);
+            ObjectInputStream obj = new ObjectInputStream(arq);
+            ArrayList<Contact> contactList = (ArrayList<Contact>) obj.readObject();
+			obj.close();
+			return contactList;
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
+        return null;
+	}
+	
+	public void saveContacts(ArrayList <Contact> contactList) {
+		try {
+			FileOutputStream arq = new FileOutputStream(path +"/" + filename);
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 	
 }
